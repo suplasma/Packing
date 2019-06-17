ebc5e5188c11da98e51784c486b8fcfccdff1944
Evgeniy Bashmakov <suplasma@ya.ru>
2019-06-17 11:56:26 +0400

    diplom

--- src/ru/suplasma/Main.java
+++ src/ru/suplasma/Main.java
@@ -1,217 +1,63 @@
 package ru.suplasma;
 
-import java.util.InputMismatchException;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = 0, i = 0, n = 0;
-        boolean flag;
+        int width = 0, height = 0, length = 0, sizeBlock[][] = null, numberOfGenes = 0, remainingAmount = 0, populations = 0;
+        readDataIn(sizeBlock, width, height, length, numberOfGenes, remainingAmount, populations);
+    }
 
-        int width = 0, height = 0, length = 0;
-
-        do {
-            try {
-                System.out.println("Ширина контейнера: ");
-                width = scanner.nextInt();
-            } catch (InputMismatchException e) {
-                flag = true;
-                System.out.println("Нужно ввести целое число, введите еще раз");
-                scanner.next();
-                continue;
+    private static int readDataIn(int[][] sizeBlock, int width, int height, int length, int numberOfGenes, int remainingAmount, int populations) {
+        ArrayList<Integer> array = new ArrayList<>();
+        int n = 0;
+        try {
+            File file = new File("dataIn.txt");
+            if (!file.exists()) {
+                System.out.println("Ошибка, файла не существует");
+                return 1;
             }
-            if (width <= 0) {
-                flag = true;
-                System.out.println("Ширина контейнера должна быть больше нуля, введите еще раз");
-            } else
-                flag = false;
-        } while (flag);
 
-        do {
-            System.out.println("Высота контейнера: ");
-            try {
-                height = scanner.nextInt();
-            } catch (Exception e) {
-                flag = true;
-                System.out.println("Нужно ввести целое число, введите еще раз");
-                scanner.next();
-                continue;
+            FileReader fr = new FileReader(file);
+            Scanner scan = new Scanner(fr);
+            width = scan.nextInt();
+            height = scan.nextInt();
+            length = scan.nextInt();
+            numberOfGenes = scan.nextInt();
+            remainingAmount = scan.nextInt();
+            populations = scan.nextInt();
+            int i = 0, k;
+            while (scan.hasNextLine()) {
+                array.add(i++, scan.nextInt());
+                array.add(i++, scan.nextInt());
+                array.add(i++, scan.nextInt());
+                k = scan.nextInt();
+                array.add(i++, k);
+                n += k;
             }
-            if (height <= 0) {
-                flag = true;
-                System.out.println("Высота контейнера должна быть больше нуля, введите еще раз");
-            } else
-                flag = false;
-        } while (flag);
-
-        do {
-            System.out.println("Длина контейнера: ");
-            try {
-                length = scanner.nextInt();
-            } catch (Exception e) {
-                flag = true;
-                System.out.println("Нужно ввести целое число, введите еще раз");
-                scanner.next();
-                continue;
-            }
-            if (length <= 0) {
-                flag = true;
-                System.out.println("Длина контейнера должна быть больше нуля, введите еще раз");
-            } else
-                flag = false;
-        } while (flag);
-
-        do {
-            System.out.println("Количество блоков: ");
-            try {
-                n = scanner.nextInt();
-            } catch (Exception e) {
-                flag = true;
-                System.out.println("Нужно ввести целое число, введите еще раз");
-                scanner.next();
-                continue;
-            }
-            if (n <= 0) {
-                flag = true;
-                System.out.println("Количество блоков должно быть больше нуля, введите еще раз");
-            } else
-                flag = false;
-        } while (flag);
-
-
-        int[][] sizeBlock = new int[n][3];
-
-        while (i < n) {
-
-            do {
-                System.out.println("Ширина блока " + (i + 1) + ":");
-                try {
-                    a = scanner.nextInt();
-                } catch (Exception e) {
-                    flag = true;
-                    System.out.println("Нужно ввести целое число, введите еще раз");
-                    scanner.next();
-                    continue;
-                }
-                if (a <= 0) {
-                    flag = true;
-                    System.out.println("Ширина блока должна быть больше нуля, введите еще раз");
-                } else if (a > width) {
-                    flag = true;
-                    System.out.println("Ширина блока не может быть больше ширины контейнера (" + width + "), введите еще раз");
-                } else
-                    flag = false;
-            } while (flag);
-
-            sizeBlock[i][0] = a;
-
-            do {
-                System.out.println("Высота блока " + (i + 1) + ":");
-                try {
-                    a = scanner.nextInt();
-                } catch (Exception e) {
-                    flag = true;
-                    System.out.println("Нужно ввести целое число, введите еще раз");
-                    scanner.next();
-                    continue;
-                }
-                if (a <= 0) {
-                    flag = true;
-                    System.out.println("Высота блока должна быть больше нуля, введите еще раз");
-                } else if (a > height) {
-                    flag = true;
-                    System.out.println("Высота блока не может быть больше высоты контейнера (" + height + "), введите еще раз");
-                } else
-                    flag = false;
-            } while (flag);
-
-            sizeBlock[i][1] = a;
-
-            do {
-                System.out.println("Длина блока " + (i + 1) + ":");
-                try {
-                    a = scanner.nextInt();
-                } catch (Exception e) {
-                    flag = true;
-                    System.out.println("Нужно ввести целое число, введите еще раз");
-                    scanner.next();
-                    continue;
-                }
-                if (a <= 0) {
-                    flag = true;
-                    System.out.println("Длина блока должна быть больше нуля, введите еще раз");
-                } else if (a > length) {
-                    flag = true;
-                    System.out.println("Длина блока не может быть больше длины контейнера (" + length + "), введите еще раз");
-                } else
-                    flag = false;
-            } while (flag);
-
-            sizeBlock[i++][2] = a;
-
+            fr.close();
+        } catch (Exception e) {
+            return 2;
         }
 
-        int numberOfGenes = 0, remainingAmount = 0, populations = 0;
+        sizeBlock = new int[n][3];
+        int k = 0;
 
-        do {
-            System.out.println("Количество особей: ");
-            try {
-                numberOfGenes = scanner.nextInt();
-            } catch (Exception e) {
-                flag = true;
-                System.out.println("Нужно ввести целое число, введите еще раз");
-                scanner.next();
-                continue;
+        for (int i = 0; i < n; ) {
+            for (int j = 0; j < array.get(k + 3); j++, i++) {
+                sizeBlock[i][0] = array.get(k);
+                sizeBlock[i][1] = array.get(k + 1);
+                sizeBlock[i][2] = array.get(k + 2);
             }
-            if (numberOfGenes <= 0) {
-                flag = true;
-                System.out.println("Количество особей должно быть больше нуля, введите еще раз");
-            } else
-                flag = false;
-        } while (flag);
+            k += 4;
+        }
 
-        do {
-            System.out.println("Отобрать особей: ");
-            try {
-                remainingAmount = scanner.nextInt();
-            } catch (Exception e) {
-                flag = true;
-                System.out.println("Нужно ввести целое число, введите еще раз");
-                scanner.next();
-                continue;
-            }
-            if (remainingAmount <= 0) {
-                flag = true;
-                System.out.println("Количество отобранных особей должно быть больше нуля, введите еще раз");
-            } else if (remainingAmount > numberOfGenes) {
-                flag = true;
-                System.out.println("Количество отобранных особей не может быть больше количества всех особей (" + numberOfGenes + "), введите еще раз");
-            } else
-                flag = false;
-        } while (flag);
-
-        do {
-            System.out.println("Популяций: ");
-            try {
-                populations = scanner.nextInt();
-            } catch (Exception e) {
-                flag = true;
-                System.out.println("Нужно ввести целое число, введите еще раз");
-                scanner.next();
-                continue;
-            }
-            if (populations <= 0) {
-                flag = true;
-                System.out.println("Популяций должно быть больше нуля, введите еще раз");
-            } else
-                flag = false;
-        } while (flag);
-
-
-        scanner.close();
 
         new Gen(sizeBlock, width, height, length, numberOfGenes, remainingAmount, populations);
+        return 0;
     }
 }
